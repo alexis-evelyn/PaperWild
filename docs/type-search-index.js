0470044d4df6ff8bfe159d002404e6fe5cdef864
Alex Evelyn <gpg@senorcontento.com>
2020-02-21 05:04:10 -0500

    Working on Documentation

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"me.alexisevelyn.paperwild","l":"Commands"},{"p":"me.alexisevelyn.paperwild","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"me.alexisevelyn.paperwild","l":"Commands"},{"p":"me.alexisevelyn.paperwild","l":"Main"},{"p":"me.alexisevelyn.paperwild","l":"ProcessChunk"},{"p":"me.alexisevelyn.paperwild","l":"Settings"}]
